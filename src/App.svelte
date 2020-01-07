638d7f5203943a302b78c2d1edb9c7d5674bebf6
cchechang <cchechang@mastercontrol.com>
2020-01-07 13:51:18 -0700

    Added some data and MeetupItem.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,26 @@
 <script>
     import Header from './UI/Header.svelte';
+
+    const meetups = [
+        {
+            id: 'm1',
+            title: 'Coding Bootcamp',
+            subtitle: 'Learn to code',
+            description: 'Experts helps you!',
+            imageUrl: 'https://www.codingdojo.com/blog/wp-content/uploads/coding-bootcamps-vs-cs-degrees.png',
+            address: '234 Rd',
+            contactEmail: 'test@code.com'
+        },
+        {
+            id: 'm2',
+            title: 'Swimming Club',
+            subtitle: 'swimming is fun',
+            description: 'Experts helps you!',
+            imageUrl: 'https://www.gotokyo.org/en/spot/1030/images/1030_0840_2_750x503.jpg',
+            address: '234 Rd',
+            contactEmail: 'swimfun@test.com'
+        },
+    ]
 </script>
 
 <Header />
